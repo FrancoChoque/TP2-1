68c09556200738bf87b203ca3f3e40d05cf2a15b
julianquino <julian.quino2@gmail.com>
2017-11-12 12:14:09 -0300

    clase test de jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -2,11 +2,12 @@ public class Jugador {
 
 
     private String nombre = "NONAME";
-
+    private int dinero; 
     public Jugador(String unNombre){
-
+    	
         this.nombre = unNombre;
 
+        this.dinero = 100000;
     }
 
 
@@ -22,9 +23,14 @@ public class Jugador {
         valordado1 = dado1.arrojar();
         valordado2 = dado2.arrojar();
 
-
         return valordado1 + valordado2;
     }
-
-
+    
+    public int getDinero(){
+    	return this.dinero;
+    }
+    
+    public void sumarDinero(int unMonto){
+    	this.dinero += unMonto;
+    }
 }
