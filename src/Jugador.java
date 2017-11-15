4e888acbb89907a5c25a68849b008715a1b5d960
FrancoChoque <franco.choquescipione@gmail.com>
2017-11-15 12:22:26 -0300

    Add files via upload

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,73 +1,93 @@
+import excepciones.*;
+
 import java.util.LinkedList;
 
 public class Jugador {
 
 	static final int DINERO_INICIAL = 100000;
-    private String nombre = "NONAME";
-    private int dinero;
+	private String nombre = "NONAME";
+	private int dinero;
 
 	private LinkedList<Comprable> propiedades;
-	private boolean movimientoPosible; 
-    
-	private int valorDados;
-    private int numeroPropiedades;
-	
+	private boolean movimientoPosible;
+
+	private int valorQueseTieneQueMover;
+	private boolean tieneQueMoverse;
+	private int numeroPropiedades;
+
+	private EstadoDeJugador jugadorEmpezandoTurno;
+	private EstadoDeJugador jugadorSinTurno;
+	private EstadoDeJugador jugadorTiroDados;
+	private EstadoDeJugador estadoDeJugador;
+
+
 	public Jugador(String unNombre){
 
-    	
-        this.nombre = unNombre;
 
-        this.dinero = DINERO_INICIAL;        
-        propiedades = new LinkedList<Comprable>();
-        
-        movimientoPosible = true;
-        
-        this.valorDados = 0;
+		this.nombre = unNombre;
+
+
+		this.dinero = DINERO_INICIAL;
+		propiedades = new LinkedList<Comprable>();
+
+		movimientoPosible = true;
+
+		this.valorQueseTieneQueMover = 0;
+		this.tieneQueMoverse = false;
 		this.numeroPropiedades = 0;
 
-    }
-    
-	public int arrojarDados(){
+		jugadorEmpezandoTurno = new JugadorEmpezandoTurno(this);
+		jugadorSinTurno = new JugadorSinTurno(this);
+		jugadorTiroDados = new JugadorTiroDados(this);
+		estadoDeJugador = jugadorSinTurno;
 
-        int valordado1;
-        int valordado2;
 
-        Dado dado1 = new Dado();
-        Dado dado2 = new Dado();
-
-        valordado1 = dado1.arrojar();
-        valordado2 = dado2.arrojar();
-        
-        this.valorDados = valordado1 + valordado2;
-        
-        return valordado1 + valordado2;
-      
-    }
-    
-    public int getDinero(){
-    	return this.dinero;
-    }
-    
-    public void setDinero(int unMonto){
-    	this.dinero = unMonto;
-    }
-    
-    public void sumarDinero(int unMonto){
-    	this.dinero += unMonto;
-    }
-
-	public void comprar(Terreno unterreno) {
-		//if(! unterreno.tieneDuenio() ) 
-			propiedades.add(unterreno);
-			unterreno.cambiarDuenio(this);
-			unterreno.pagarCompra(this) ;
-		
 	}
 
+	public int arrojarDados() throws NoEsTurnoJugador, JugadorYaTiroDados {
 
+		return estadoDeJugador.arrojarDados();
 
+	}
 
+	public int getDinero(){
+		return this.dinero;
+	}
 
+	public void setDinero(int unMonto){
+		this.dinero = unMonto;
+	}
+
+	public void sumarDinero(int unMonto){
+		this.dinero += unMonto;
+	}
+
+	public void comprar(Terreno unTerreno) throws NoEsTurnoJugador, JugadorYaTiroDados {
+		estadoDeJugador.comprar(unTerreno);
+
+	}
+
+	public void setEstado(EstadoDeJugador unEstado){
+
+		this.estadoDeJugador = unEstado;
+
+	}
+
+	public EstadoDeJugador getEstadoDeJugador() {
+		return estadoDeJugador;
+	}
+
+	public EstadoDeJugador getJugadorEmpezandoTurno() {
+		return jugadorEmpezandoTurno;
+	}
+
+	public EstadoDeJugador getJugadorSinTurno() {
+		return jugadorSinTurno;
+	}
+
+	public EstadoDeJugador getJugadorTiroDados() {
+		return jugadorTiroDados;
+	}
 
 	public boolean puedeMoverse() {
 		// TODO Auto-generated method stub
@@ -75,7 +95,10 @@ public class Jugador {
 	}
 
 
+	public void adquirirPropiedad(Terreno unTerreno){
 
+		this.propiedades.add(unTerreno);
+	}
 
 
 	public void cambiarMovimiento() {
@@ -89,26 +112,42 @@ public class Jugador {
 		// TODO Auto-generated method stub
 		return dinero >= monto;
 	}
-	
-	public void setValorDados(int valor){
-    	this.valorDados = valor;
-    }
-    
-    public int getValorDados(){
-    
-    	return this.valorDados;
-    }
-    
-    public int getNumeroPropiedades() {
+
+	public void setValorQueseTieneQueMover(int valor){
+		this.valorQueseTieneQueMover = valor;
+	}
+
+	public int getValorQueseTieneQueMover(){
+
+		return this.valorQueseTieneQueMover;
+	}
+
+	public void comenzarAmoverse(){
+		this.tieneQueMoverse = true;
+	}
+
+	public void continuarMoviendose(){
+		this.tieneQueMoverse = true;
+	}
+
+	public boolean getTieneQueMoverse(){
+		return this.tieneQueMoverse;
+	}
+
+	public void seMovio(){
+		this.tieneQueMoverse = false;
+	}
+
+	public int getNumeroPropiedades() {
 		return numeroPropiedades;
 	}
 
 	public void setNumeroPropiedades(int numPropiedades) {
 		this.numeroPropiedades = numPropiedades;
 	}
-	
+
 	public void aumentarNumeroDePropiedades(int unAumento){
 		this.numeroPropiedades += unAumento;
 	}
-	
+
 }
