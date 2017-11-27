07f7d422a66e84cc62089e109b80d48d042eb452
Nicolas.Simizu <nicosimi993@gmail.com>
2017-11-27 10:19:07 -0300

    Agrego pruebas 1 y 2

--- test/Entrega3/Prueba302Test.java
+++ test/Entrega3/Prueba302Test.java
@@ -1,18 +1,19 @@
 package Entrega3;
 
-import modelo.*;
-import modelo.Jugador.Jugador;
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 import excepciones.JugadorYaTiroDados;
 import excepciones.NoEsTurnoJugador;
+import modelo.Jugador.Jugador;
+import modelo.*;
 
-public class Prueba301Test {
+public class Prueba302Test {
 
-    @Test
-
-    public void test(){
+	@Test
+	public void test(){
     	AlgoPoly algopoly = new AlgoPoly();
 		Jugador jugador = algopoly.nuevoJugador("jugador1");
 		jugador.ponerDadosIguales();
@@ -20,12 +21,13 @@ public class Prueba301Test {
 		
 		try {
 			algopoly.usarTurno(jugador);
+			algopoly.usarTurno(jugador);
 		} catch (NoEsTurnoJugador | JugadorYaTiroDados e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		Assert.assertEquals(jugador.getJugadorEmpezandoTurno() , jugador.getEstadoDeJugador() );
+		Assert.assertEquals(jugador.getJugadorTiroDados() , jugador.getEstadoDeJugador() );
     }
 
 }
