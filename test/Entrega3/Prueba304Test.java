5dac890db5dcec1448c06a6bcfe782e4fa419ca2
Nicolas.Simizu <nicosimi993@gmail.com>
2017-11-27 09:45:35 -0300

    Subo prueba 304

---
+++ test/Entrega3/Prueba304Test.java
@@ -0,0 +1,30 @@
+package Entrega3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import excepciones.JugadorYaTiroDados;
+import excepciones.NoEsTurnoJugador;
+import modelo.AlgoPoly;
+import modelo.Jugador.Jugador;
+
+public class Prueba304Test {
+
+	@Test
+	public void test() {
+		AlgoPoly algopoly = new AlgoPoly();
+		Jugador jugador = algopoly.nuevoJugador("jugador1");
+	
+		try {
+			algopoly.usarTurno(jugador);
+		} catch (NoEsTurnoJugador | JugadorYaTiroDados e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertEquals(jugador.getValorDados(), algopoly.obtenerPosicion(jugador));
+	}
+
+}
